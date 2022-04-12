016c5558d596d59eb847d011c94789656f10e3d2
Stu Wares <stuwares@outlook.com>
2022-04-12 17:22:01 +0100

    Dork mode added

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const taskList = ["Log out and back in","Open My Team Listing","Edit an emergency contact","View job chats","Pick a report and try to run it"];
 const reportList = ["Call List - Select Depot","Call List - Discipline/Absence...","Absence - Select Employee","Disciplinary - Select Emplolyee","Absences by Employee","Absences No End Dates","Age Report Enter min and...","Complaints by Route","Holiday Balance Report","Leavers Report between Dates","Disciplinary Report by Depot...","Commendations and Complaints by..."]
 
+let matched = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+if(matched) {
+	console.log('Currently in dark mode');
+    document.getElementById("mainBody").style.background = 'black';
+    document.getElementById("titleText").style.color = 'white';
+}else {
+	console.log('Currently not in dark mode');
+}
+
+
 function generateNewTask() {
     let randoNumber = Math.floor(Math.random() * 6);
 
